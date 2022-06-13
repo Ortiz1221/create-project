c11924821cccab112acc4c213f582995aae2eed6
Tatiana Klevanets <t.klevanets@gmail.com>
2022-06-13 17:33:59 +0300

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,48 @@
+import chalk from 'chalk';
+import Listr from 'listr';
+import path from 'path';
+
+import { copyTemplateFiles } from './utils/copy-template-files';
+import { initGitRepo } from './utils/init-git-repo';
+import { installPackages } from './utils/install-packages';
+
+import type { Options } from './types';
+
+export async function createProject(options: Options) {
+    const targetDirectory = process.cwd();
+    const currentFileUrl = import.meta.url;
+    const templateDirectory = path.resolve(
+        decodeURI(new URL(currentFileUrl).pathname),
+        '../../templates',
+        options.template.toLowerCase()
+    );
+
+    const tasks = new Listr([
+        {
+            title: 'Copy project files',
+            task: () => copyTemplateFiles(templateDirectory, targetDirectory)
+        },
+        {
+            title: 'Initialize git',
+            task: () => initGitRepo(targetDirectory),
+            enabled: () => options.git
+        },
+        {
+            title: 'Install dependencies',
+            task: () => installPackages(targetDirectory),
+            skip: () => {
+                if (!options.install) {
+                    return 'Pass --install or -i to automatically install dependencies';
+                }
+            }
+        }
+    ]);
+
+    try {
+        await tasks.run();
+
+        console.log('%s Project ready', chalk.green.bold('DONE'));
+    } catch (error) {
+        console.log('%s Error occurred', chalk.red.bold('ERROR'));
+    }
+}
---
+++ src/utils/parse-arguments-into-options.ts
@@ -0,0 +1,42 @@
+import arg from 'arg';
+import chalk from 'chalk';
+
+import { checkTemplateValidity } from './check-template-validity';
+import { templates } from '../types';
+
+import type { Args, RawOptions } from '../types';
+
+export function parseArgumentsIntoOptions(rawArgs: Args): RawOptions {
+    const args = arg(
+        {
+            '--git': Boolean,
+            '--yes': Boolean,
+            '--install': Boolean,
+            '-g': '--git',
+            '-y': '--yes',
+            '-i': '--install'
+        },
+        {
+            argv: rawArgs.slice(2)
+        }
+    );
+
+    const template = args._[0]?.toLowerCase();
+    const isTemplateValid = checkTemplateValidity(template);
+
+    if (!isTemplateValid) {
+        console.log(
+            `%s You passed incorrect template: ${
+                args._[0]
+            }. List of supported templates: ${templates.join(', ')}`,
+            chalk.yellow.bold('WARNING')
+        );
+    }
+
+    return {
+        git: args['--git'] || false,
+        install: args['--install'] || false,
+        skipPrompts: args['--yes'] || false,
+        template: isTemplateValid ? template : undefined
+    };
+}
